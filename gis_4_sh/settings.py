1bbc4675ec1c5af9e0bbb31e73be845066adbd41
kim sung hoon <tjdtjdtl0514@likelion.org>
2021-07-27 13:13:36 +0900

    부트스트랩4

--- gis_4_sh/settings.py
+++ gis_4_sh/settings.py
@@ -54,6 +54,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'bootstrap4',
     'accountapp',
+    'profileapp',
+
 ]
 
 MIDDLEWARE = [
@@ -133,9 +135,16 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
